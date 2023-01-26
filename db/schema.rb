362f4506501b82cc3131ecd02f1fde96b046a871
aakbarkhan <aakuukhan@gmail.com>
2023-01-26 20:17:56 +0530

    made react and js functional

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_19_094258) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_25_082917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,8 +49,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_19_094258) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "line_items", "carts"
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
 end
-
